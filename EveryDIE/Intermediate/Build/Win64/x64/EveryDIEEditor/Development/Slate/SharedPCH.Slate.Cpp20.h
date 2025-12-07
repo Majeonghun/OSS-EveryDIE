a82b7f826ffae2ac1e11db2162042cc0a4575d02
Alt+F4 <133301559+Majeonghun@users.noreply.github.com>
2025-12-08 04:22:53 +0900

    Beta Version
    Update the project (최적화 위주), 오픈 소스 최적화

--- EveryDIE/Intermediate/Build/Win64/x64/EveryDIEEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Slate/Public/SlateSharedPCH.h
-#include "C:/OSS-EveryDIE/EveryDIE/Intermediate/Build/Win64/x64/EveryDIEEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
-#include "Runtime/Slate/Public/SlateSharedPCH.h"
